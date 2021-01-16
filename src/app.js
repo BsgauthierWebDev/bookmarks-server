704fd2ba48f6c290a20d94b0be259b9cb8480a84
Brent Gauthier <72673424+BsgauthierWebDev@users.noreply.github.com>
2021-01-16 15:57:22 -0500

    Add files via upload
    Updated to handle malicious code

--- src/app.js
+++ src/app.js
@@ -3,26 +3,24 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const {NODE_ENV} = require('./config')
+const { NODE_ENV } = require('./config')
 const validateBearerToken = require('./validate-bearer-token')
-const bookmarksRouter = require('./bookmarks/bookmarks-router')
 const errorHandler = require('./error-handler')
+const bookmarksRouter = require('./bookmarks/bookmarks-router')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common';
-
-app.use(morgan(morganOption))
-app.use(helmet())
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
 app.use(cors())
+app.use(helmet())
 app.use(validateBearerToken)
 
 app.use(bookmarksRouter)
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
+  res.send('Hello, world!')
 })
 
 app.use(errorHandler)
